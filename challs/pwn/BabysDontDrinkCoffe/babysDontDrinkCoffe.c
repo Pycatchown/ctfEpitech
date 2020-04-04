8b8ed83a3607aae52c12a6e8830abc2a455826f7
Baptiste Mayaud <baptiste.mayaud@epitech.eu>
2020-04-04 20:02:22 +0200

    BabysDontDrinkCoffe completed, with solution and dockerfile

---
+++ challs/pwn/BabysDontDrinkCoffe/babysDontDrinkCoffe.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+void I_l1k3_my_c0ffe(void)
+{
+    system("/bin/sh");
+}
+
+int main(void)
+{
+    int c00ff3 = 0;
+    short pl4t3 = 2;
+    char f0rk = 5;
+    char t4bl3[30] = "This an awesome table!";
+    int br00m = 0xdeadbeef;
+    char buffer[124];
+
+    gets(buffer);
+
+    if (strcmp(t4bl3, "This an awesome table!")) {
+        puts("You made a mess again!");
+        return (0);
+    }
+    if (pl4t3 == 2 && f0rk == 5 && br00m == 0xdeadbeef && c00ff3 == 0xc0ffe)
+        I_l1k3_my_c0ffe();
+    else
+        puts("You made a mess again!");
+
+    return (0);
+}
